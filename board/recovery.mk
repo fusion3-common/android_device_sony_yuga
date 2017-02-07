12b752cc6c9508509ed2f2fde36df47a715e19f8
Roman Yarullin <firefox883@gmail.com>
2017-02-07 15:23:55 +0000

    yuga: Add support for TWRP
    Change-Id: If6714a9d5b9853074babd246d6d71164a9a9720e

---
+++ board/recovery.mk
@@ -0,0 +1,6 @@
+# TWRP Support (Optional)
+ifeq ($(WITH_TWRP),true)
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+TW_BRIGHTNESS_PATH := /sys/class/leds/lm3533-lcd-bl/brightness
+TW_THEME := portrait_hdpi
+endif
